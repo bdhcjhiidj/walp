6738731c892a30b36824a45d1db0401a47145297
Dark-Senpai <87824804+Dark-Senpai@users.noreply.github.com>
2021-11-04 15:19:12 +0530

    Update __main__.py
    🤡🤭

--- bot/__main__.py
+++ bot/__main__.py
@@ -39,11 +39,11 @@ from bot.plugins.status_message_fn import (
 from bot.commands import Command
 from bot.plugins.call_back_button_handler import button
 sudo_users = "1666551439" 
-crf.append("29.8")
+crf.append("28.6")
 codec.append("libx265")
-resolution.append("854x480")
+resolution.append("1280x720")
 preset.append("veryfast")
-audio_b.append("50k")
+audio_b.append("40k")
 # 🤣
 
 
