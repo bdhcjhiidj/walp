32ed4dfc4f2a650b3fe604030d63612f145c3e44
krispychip1 <84122709+krispychip1@users.noreply.github.com>
2021-11-11 07:45:20 +0530

    Update incoming_message_fn.py

--- bot/plugins/incoming_message_fn.py
+++ bot/plugins/incoming_message_fn.py
@@ -36,7 +36,7 @@ from pyrogram.errors.exceptions.bad_request_400 import UserNotParticipant, Usern
 #from bot.helper_funcs.utils import(
 #  delete_downloads
 #)
-os.system("wget https://telegra.ph/file/299491b31add580b46b3b.jpg -O thumb.jpg")
+os.system("wget https://telegra.ph/file/0f3fcdea968319a1a48d6.jpg -O thumb.jpg")
 
 #LOGS_CHANNEL = -1001283278354
 CURRENT_PROCESSES = {}
