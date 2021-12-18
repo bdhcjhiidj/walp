460eac66c1d82e0c24230dc562a076a2bf19b2cf
Krispchip3 <94070025+Krispchip3@users.noreply.github.com>
2021-12-18 18:43:35 +0530

    Update __main__.py

--- bot/__main__.py
+++ bot/__main__.py
@@ -42,7 +42,7 @@ sudo_users = "1666551439"
 crf.append("28")
 codec.append("libx264")
 resolution.append("800x400")
-preset.append("ultrafast")
+preset.append("veryfast")
 audio_b.append("30k")
 # 🤣
 
