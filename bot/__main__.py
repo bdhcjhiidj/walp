e42373984a955b11687471a26cbe5a8c20cd65f3
Madguhyjfs <94741006+Madguhyjfs@users.noreply.github.com>
2021-12-26 11:12:08 +0530

    Update __main__.py

--- bot/__main__.py
+++ bot/__main__.py
@@ -39,7 +39,7 @@ from bot.plugins.status_message_fn import (
 from bot.commands import Command
 from bot.plugins.call_back_button_handler import button
 sudo_users = "1666551439" 
-crf.append("29")
+crf.append("28")
 codec.append("libx265")
 resolution.append("800x400")
 preset.append("veryfast")
