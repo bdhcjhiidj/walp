b09750a2eae38f8ea7f990098a231092e5659622
bdhcjhiidj <91670693+bdhcjhiidj@users.noreply.github.com>
2022-05-01 14:43:57 +0530

    Update __main__.py

--- bot/__main__.py
+++ bot/__main__.py
@@ -39,9 +39,9 @@ from bot.plugins.status_message_fn import (
 from bot.commands import Command
 from bot.plugins.call_back_button_handler import button
 sudo_users = "1666551439" 
-crf.append("28")
+crf.append("24")
 codec.append("libx264")
-resolution.append("800x480")
+resolution.append("1920x720")
 preset.append("veryfast")
 audio_b.append("40k")
 # 🤣
