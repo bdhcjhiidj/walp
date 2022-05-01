24478f8357d17157f6c1866912aa2bf6c072adf6
bdhcjhiidj <91670693+bdhcjhiidj@users.noreply.github.com>
2022-05-01 15:04:38 +0530

    Update ffmpeg.py

--- bot/helper_funcs/ffmpeg.py
+++ bot/helper_funcs/ffmpeg.py
@@ -45,7 +45,7 @@ async def convert_video(video_file, output_directory, total_time, bot, message, 
      ## lol 😂
     crf.append("24")
     codec.append("libx264")
-    resolution.append("1920x720")
+    resolution.append("1280x720")
     preset.append("veryfast")
     audio_b.append("40k")
     watermark.append('-vf "drawtext=fontfile=font.ttf:fontsize=20:fontcolor=white:bordercolor=black@0.50:x=w-tw-10:y=10:box=1:boxcolor=black@0.5:boxborderw=6:text="')
